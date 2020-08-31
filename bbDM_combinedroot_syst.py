2d20f1242b10f521483df2097306075e8efd6cfa
Praveen Chandra Tiwari <physics.pctiwari@gmail.com>
2020-08-31 22:04:06 +0530

    updated allmet histo maker code

--- bbDM_combinedroot_syst.py
+++ bbDM_combinedroot_syst.py
@@ -1,42 +1,94 @@
-import sys,os,array
-
+import sys
+import os
+import optparse
+import argparse
 from array import array
 from glob import glob
-
 from ROOT import TFile, gROOT, kBlack,TH1F
 
-import signal_xsec_list as xsec_dict
+usage = "usage: %prog [options] arg1 arg2"
+parser = optparse.OptionParser(usage)
+
+parser.add_option("-D", "--pDir", type="string",
+                  dest="rootFileDir", help="directory containing histogram")
+parser.add_option("-S", "--sigDir", type="string", dest="SIGrootFileDir",
+                  help="directory containing signal histogram")
+parser.add_option("-t", "--tag", type="string",
+                  dest="plot_tag", help="version of histogram")
+parser.add_option("-e", "--era", type="string",
+                  dest="era_year", help="year of histogram")
+
+(options, args) = parser.parse_args()
+
+runOn2016 = False
+runOn2017 = False
+runOn2018 = False
+
+if options.era_year == '2016':
+    runOn2016 = True
+elif options.era_year == '2017':
+    runOn2017 = True
+elif options.era_year == '2018':
+    runOn2018 = True
+else:
+    print('Please provide on which year you want to run?')
+
+if runOn2016:
+    import sig_sample_xsec_2016 as xsec_dict
+    luminosity = 35.82 * 1000
+    era_name = 'bbDM2016_'
+elif runOn2017:
+    import sig_sample_xsec_2017 as xsec_dict
+    luminosity = 41.5 * 1000
+    era_name = 'bbDM2017_'
+elif runOn2018:
+    import sig_sample_xsec_2018 as xsec_dict
+    luminosity = 59.64 * 1000
+    era_name = 'bbDM2018_'
 
 gROOT.SetBatch(True)
 
+if options.rootFileDir == None:
+    print('Please provide histogram directory name')
+    sys.exit()
+else:
+    CRSRPath = options.rootFileDir
+
+if options.SIGrootFileDir == None:
+    print('Please provide signal histogram directory name')
+    sys.exit()
+else:
+    SignalPath = options.SIGrootFileDir
+
+if options.plot_tag == None:
+    print('Please provide histogram directory name')
+    sys.exit()
+else:
+    plot_tag = options.plot_tag
+
 #CRSRPath = '/Users/dekumar/MEGA/Fullwork/2017_Plotting/22102019/monoHROOT'
-CRSRPath = '/Users/ptiwari/cernbox/Documents/plottingTools/plots_norm/11032020_2016/bbDMRoot'
+#CRSRPath = '/Users/ptiwari/cernBox/Documents/ExoPieCapper/plots_norm/analysis_plots_v18_06-04-00-HemAtSkim/bbDMRoot'
 #SignalPath = '/Users/dekumar/MEGA/Fullwork/2017_Plotting/rootFiles_Signal'
-SignalPath = '/Users/ptiwari/cernbox/Documents/plottingTools/2016_syst_sig'
+#SignalPath = '/Users/ptiwari/cernBox/Documents/ExoPieCapper/analysis_histo_v18_06-04-00-HemAtSkim'
 
 CRSRFiles = [CRSRPath+'/'+fl for fl in os.listdir(CRSRPath) if 'Recoil' in fl or 'MET' in fl]
-SignalFiles = [SignalPath+'/'+fl for fl in os.listdir(SignalPath) if '.root' in fl]
+SignalFiles = [SignalPath+'/' +fl for fl in os.listdir(SignalPath) if '.root' in fl and 'bbDM_2HDMa' in fl]
 
-os.system('rm -rf DataCardRootFiles')
-os.system('mkdir DataCardRootFiles')
+# os.system('rm -rf DataCardRootFiles')
+# os.system('mkdir DataCardRootFiles')
 
 
 def setHistStyle(h_temp,newname):
-
     #h_temp=h_temp2.Rebin(len(bins)-1,"h_temp",array.array('d',bins))
     h_temp.SetName(newname)
     h_temp.SetTitle(newname)
     h_temp.SetLineWidth(1)
-    #h_temp.SetBinContent(len(bins)-1,h_temp.GetBinContent(len(bins)-1)+h_temp.GetBinContent(len(bins))) #Add overflow bin content to last bin
-    #h_temp.SetBinContent(len(bins),0.)
-    #h_temp.GetXaxis().SetRangeUser(200,1000)
-    h_temp.SetMarkerColor(kBlack);
-    h_temp.SetMarkerStyle(2);
+    h_temp.SetMarkerColor(kBlack)
+    h_temp.SetMarkerStyle(2)
     return h_temp
 
 
 def reBin(h_temp,bins):
-
     h_temp=h_temp.Rebin(len(bins)-1,"h_temp",array.array('d',bins))
     #h_temp.SetBinContent(len(bins)-1,h_temp.GetBinContent(len(bins)-1)+h_temp.GetBinContent(len(bins))) #Add overflow bin content to last bin
     #h_temp.SetBinContent(len(bins),0.)
@@ -46,8 +98,6 @@ def reBin(h_temp,bins):
     return h_temp
 
 
-#xsec_dict.CSList_150 = {'ma_150_mA_300':1.606,'ma_150_mA_400':0.987,'ma_150_mA_500':0.5074,'ma_150_mA_600':0.2984,'ma_150_mA_1000':0.0419,'ma_150_mA_1200':0.0106,'ma_150_mA_1600':0.07525}
-
 
 print ('xsec_dict.CSList_0',xsec_dict.CSList_0)
 
@@ -55,7 +105,7 @@ SRCRhistos=['bkgSum','DIBOSON','ZJets','GJets','QCD','STop','Top','WJets','DYJet
 
 bins= [200,250,350,500,1000]
 
-f=TFile("DataCardRootFiles/AllMETHistos.root","RECREATE")
+f=TFile("DataCardRootFiles/AllMETHistos_"+plot_tag+".root","RECREATE")
 
 for infile in CRSRFiles:
     print ('checking code for ',infile)
@@ -69,12 +119,12 @@ for infile in CRSRFiles:
         syst    = '_'+laststr.split("_")[-2]+'_'+laststr.split("_")[-1].replace('.root','')
     if ('MET' in infile and 'SR' not in infile):continue# or ('Recoil' not in infile): continue
     print ('running code for ',infile)
-    reg = reg.replace('Zmumu','ZMUMU').replace('Zee','ZEE').replace('Wmunu','WMUNU').replace('Wenu','WENU').replace('Topmunu','TOPMUNU').replace('Topenu','TOPENU')
+    reg = reg.replace('ZmumuCR','ZMUMU').replace('ZeeCR','ZEE').replace('WmunuCR','WMU').replace('WenuCR','WE').replace('TopmunuCR','TOPMU').replace('TopenuCR','TOPE')
 
     for hist in SRCRhistos:
         temp   = fin.Get(hist)
         hist=hist.replace('DIBOSON','diboson').replace('ZJets','zjets').replace('GJets','gjets').replace('QCD','qcd').replace('STop','singlet').replace('Top','tt').replace('WJets','wjets').replace('DYJets','dyjets')
-        newName   = 'bbDM2016_'+reg+'_'+str(hist)+syst
+        newName   = era_name+reg+'_'+str(hist)+syst
         if not syst=='' and hist=='data_obs':continue
         if temp.Integral() == 0.0:
             HISTNAME=newName
@@ -83,27 +133,29 @@ for infile in CRSRFiles:
             # print ('=================',temp.GetXaxis().GetNbins())
             for bin in range(4):
                 temp.SetBinContent(bin+1,0.00001)
+                if temp.GetBinError(bin)<0:
+                    temp.SetBinError(bin,0.0)
+
+        for bin in range(temp.GetXaxis().GetNbins()):
+            if temp.GetBinContent(bin+1)==0:
+                temp.SetBinContent(bin+1,0.00001)
+            if temp.GetBinError(bin)<0:
+                temp.SetBinError(bin,0.0)
 
         myHist = setHistStyle(temp,newName)
         f.cd()
         myHist.Write()
 
-
-lumi = 35.8*1000
-
-BR = 0.588
-
 for cat in ['1b','2b']:
     for infile in SignalFiles:
         #print ('infile',infile)
-        fin       =   TFile(infile,"READ")
+        fin = TFile(infile,"READ")
         rootFile = infile.split('/')[-1]
         #print ('rootFile', rootFile.split('_'))
         ma=rootFile.split('_')[6].strip('Ma')
         mA=rootFile.split('_')[8].strip('MA')
-
-        if mA=='1400': continue
-
+        if int(mA)==1200:
+            print (ma)
         sampStr = 'Ma'+ma+'_MChi1_MA'+mA
         #Ma250_MChi1_MA1200
         CS = xsec_dict.CSList_0[sampStr]
@@ -114,12 +166,20 @@ for cat in ['1b','2b']:
                 if  temp.Integral() == 0.0:
                     for bin in range(temp.GetXaxis().GetNbins()):
                         temp.SetBinContent(bin,0.00001)
+                    if temp.GetBinError(bin)<0:
+                        temp.SetBinError(bin,0.0)
+
+                for bin in range(temp.GetXaxis().GetNbins()):
+                    if temp.GetBinContent(bin)==0:
+                        temp.SetBinContent(bin,0.00001)
+                    if temp.GetBinError(bin)<0:
+                        temp.SetBinError(bin,0.0)
 
                 h_total = fin.Get('h_total_mcweight')
                 totalEvents = h_total.Integral()
-                temp.Scale((lumi*CS*BR)/(totalEvents))
-                samp = 'bbDM2016_'+cat+'_SR_2HDMa_Ma'+ma+'_MChi1_MA'+mA+'_tb35_st_0p7'
-                #samp = 'bbDM2016_'+cat+'_SR_2HDMa_Ma'+ma+'_MChi1_MA'+mA
+                temp.Scale((luminosity*CS)/(totalEvents))
+                samp = era_name+cat+'_SR_2HDMa_Ma'+ma+'_MChi1_MA'+mA+'_tb35_st_0p7'
+                #samp = era_name+cat+'_SR_2HDMa_Ma'+ma+'_MChi1_MA'+mA
 
                 myHist = setHistStyle(temp,samp)
                 f.cd()
@@ -131,16 +191,21 @@ for cat in ['1b','2b']:
                     if  temp.Integral() == 0.0:
                         for bin in range(temp.GetXaxis().GetNbins()):
                             temp.SetBinContent(bin,0.00001)
-
+                            if temp.GetBinError(bin)<0:
+                                temp.SetBinError(bin,0.0)
+                    for bin in range(temp.GetXaxis().GetNbins()):
+                        if temp.GetBinContent(bin)==0:
+                            temp.SetBinContent(bin,0.00001)
+                        if temp.GetBinError(bin)<0:
+                            temp.SetBinError(bin,0.0)
                     h_total = fin.Get('h_total_mcweight')
                     totalEvents = h_total.Integral()
-                    temp.Scale((lumi*CS*BR)/(totalEvents))
-                    samp = 'bbDM2016_'+cat+'_SR_2HDMa_Ma'+ma+'_MChi1_MA'+mA+'_tb35_st_0p7_'+syst+'_'+ud
-                    #samp = 'bbDM2016_'+cat+'_SR_2HDMa_Ma'+ma+'_MChi1_MA'+mA
+                    temp.Scale((luminosity*CS)/(totalEvents))
+                    samp = era_name+cat+'_SR_2HDMa_Ma'+ma+'_MChi1_MA'+mA+'_tb35_st_0p7_'+syst+'_'+ud
+                    #samp = era_name+cat+'_SR_2HDMa_Ma'+ma+'_MChi1_MA'+mA
 
                     myHist = setHistStyle(temp,samp)
                     f.cd()
                     myHist.Write()
-
-
+    print('\n')
 f.Close()
